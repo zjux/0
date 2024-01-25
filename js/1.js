f8254fdbab7948a2dfe8fdbb2738ee090259f559
zjux <146630056+zjux@users.noreply.github.com>
2024-01-25 22:27:30 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -49,7 +49,7 @@ var rule = {
             d.push({
                 title: it.title,
                 img: it.path,
-                desc: it.score + '分 / ' + it.year.title,
+                desc: it.score + '分 / ' + it.year,
                 url: it.id
             })
         });
@@ -67,7 +67,7 @@ var rule = {
             d.push({
                 title: it.title,
                 img: it.thumbnail||it.path,
-		desc: it.score + '分 / ' + it.year.title,
+		desc: it.score + '分 / ' + it.year,
                 url: it.id
             })
         });
@@ -125,7 +125,7 @@ var rule = {
             d.push({
                 title: it.title,
                 img: it.thumbnail,
-		desc: it.score + '分 / ' + it.year.title,
+		desc: it.score + '分 / ' + it.year,
                 url: it.id
             })
         });
